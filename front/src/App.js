5f8f91d8785abe8f217cb7aef6f6e01b9d10d3ac
Jitae Kim <originerd@gmail.com>
2019-01-11 23:45:53 +0900

    Add UserList to App

--- front/src/App.js
+++ front/src/App.js
@@ -1,10 +1,21 @@
 import React from 'react';
 
 import SignIn from './SignIn';
+import UserList from './UserList';
 
 class App extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = { username: undefined };
+  }
+
   render() {
-    return <SignIn />;
+    if (!this.state.username) {
+      return <SignIn />;
+    }
+
+    return <UserList />;
   }
 }
 
--- front/src/App.test.js
+++ front/src/App.test.js
@@ -3,13 +3,32 @@ import React from 'react';
 
 import App from './App';
 import SignIn from './SignIn';
+import UserList from './UserList';
 
 describe('App', () => {
-  it('renders SignIn', () => {
-    // When
-    const wrapper = shallow(<App />);
+  describe('when username is not set', () => {
+    it('renders SignIn', () => {
+      // When
+      const wrapper = shallow(<App />);
+  
+      // Then
+      expect(wrapper.find(SignIn).length).toBe(1);
+      expect(wrapper.find(UserList).length).toBe(0);
+    });
+  });
 
-    // Then
-    expect(wrapper.find(SignIn).length).toBe(1);
+  describe('when username is set', () => {
+    it('renders UserList', () => {
+      // Given
+      const wrapper = shallow(<App />);
+      const username = 'Jitae Kim';
+
+      // When
+      wrapper.setState({ username });
+  
+      // Then
+      expect(wrapper.find(SignIn).length).toBe(0);
+      expect(wrapper.find(UserList).length).toBe(1);
+    });
   });
 });
